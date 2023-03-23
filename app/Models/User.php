ea73e6240ca3616adef0b8188d3da8f6eb150dbe
andcon96 <andrew.conan1996@gmail.com>
2023-03-23 16:53:07 +0700

    User

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,10 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    protected $connection = 'mysql2';
+
+    public $table = 'mst_anggota';
+    protected $primaryKey = 'id_anggota';
 
     public function findForPassport($identifier) {
         return $this->orWhere('email', $identifier)->orWhere('nik', $identifier)->first();
